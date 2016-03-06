326eb96a1af98fc6ab704348325552c3fd03bec2
Valentin Mladenov <hudsonvsm@gmail.com>
2016-03-06 14:25:08 +0200

    Krasen's task.

---
+++ graph-work/Krasens_task.cpp
@@ -0,0 +1,24 @@
+//
+// Created by hudson on 6.3.2016 г..
+//
+
+using namespace std;
+
+const int graphElements = 5;
+struct node {
+    char key;
+    node *next;
+} *graph[graphElements];
+
+void delete_node_with_max_value (node *graph[graphElements]){
+    char maxKey = graph[0]->key;
+    for (int i = 0; i < graphElements; ++i) {
+        if (graph[i] != nullptr) {
+            if (graph[i]->key > maxKey) {
+                maxKey = graph[i]->key;
+            }
+        }
+    }
+
+    delete_node(graph, maxKey);
+}
