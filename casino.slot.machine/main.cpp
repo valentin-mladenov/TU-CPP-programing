a1cde42e142a6b3a785a76685ad06e6c808be21d
Valentin Mladenov <hudsonvsm@gmail.com>
2016-10-08 13:13:41 +0300

    Finished.

--- casino.slot.machine/main.cpp
+++ casino.slot.machine/main.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 #include <cstdlib>
 #include <algorithm>
-#include <iterator>
-#include <string>
-#include <array>
-#ifdef WIN32
-#include <windows.h>
-#else
-#include <unistd.h>
-#endif // win32
+#include <chrono>
 
 using namespace std;
 int winnerDigits[10] = { 0, 111, 222, 333, 444, 555, 666, 777, 888, 999 };
 
 int main() {
-    int command, tempBet = 0, bet = 0, tempMoney = 0, money = 0, number = 0;
-    string breakInput;
+    int command, haveEnoughMoney, tempBet = 0, bet = 0, tempMoney = 0, money = 0, number;
+    double seconds;
+    time_t start;
+    bool exists;
 
     while (true) {
         cout << endl << endl << "Main Menu" << endl;
@@ -27,6 +22,9 @@ int main() {
         cout << "4 -  EXIT" << endl;
         cout << "\n\n Chose command (1-4): ";
         cin >> command;
+
+        // clear console
+        if (system("CLS")) system("clear");
 
         switch (command) {
             case 1:
@@ -44,12 +42,12 @@ int main() {
                 cout << "Bet up to 10: ";
                 cin >> tempBet;
 
-                int areEnoughMoney;
-                areEnoughMoney = money - tempBet;
+                haveEnoughMoney = money - tempBet;
 
-                if ((tempBet > 0 || tempBet < 11) && (areEnoughMoney >= 0)) {
+                if ((tempBet > 0 || tempBet < 11) && (haveEnoughMoney >= 0)) {
                     bet = tempBet;
-                } else if (areEnoughMoney < 0) {
+                    money -= bet;
+                } else if (haveEnoughMoney < 0) {
                     cout << (money - tempBet) << "Not enough money for that bet.";
                 } else {
                     cout << (money - tempBet) << "Incorrect amount. Must be from 1 to 10. ";
@@ -61,43 +59,24 @@ int main() {
                     cout  << "Place a bet. Current bet is 0.";
                     break;
                 }
-                while (cin.get()!='\n')
-                    ;
 
-                //breakInput = 1;
-                while (true) {
-                    // array<char, 1> buffer;
-                    // cin.clear();
-                    //cin.ignore(1, '\n');
-                    //cin.readsome(buffer.data(), 1);
+                seconds = 3 + ( rand() % ( 10 - 3 + 1 ) );
+                start = time(0);
 
-                    auto result = cin.get() != -52;
-                    //cout<<cin.get()<<endl;
-                    //cout<<"check "<<result<<endl;
-//
-                    if (result) break;
-
+                while (difftime(time(0), start) <= seconds) {
                     number = rand() % 1000;
                     cout << "\r" << number;
-#ifdef WIN32
-                    Sleep(100);
-#else
-                    sleep(200);
-#endif // win32
                 }
 
-                bool exists;
                 exists = find(begin(winnerDigits), end(winnerDigits), number) != end(winnerDigits);
 
                 if (exists) {
                     money = money + (bet * 10);
-                    cout << "WIN!!!";
+                    cout << "\nWIN!!!";
                 } else {
-                    money -= bet;
-                    cout << "You have lost.";
+                    cout << "\nYou have lost.";
                 }
                 bet = 0;
-
                 break;
             case 4:
                 return 0;
